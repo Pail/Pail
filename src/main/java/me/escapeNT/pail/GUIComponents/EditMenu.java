77639ccbc8523624cc67166315e427245e822314
escapeNT <auxadress@gmail.com>
2011-09-04 12:00:06 -0400

    Clear console

--- src/main/java/me/escapeNT/pail/GUIComponents/EditMenu.java
+++ src/main/java/me/escapeNT/pail/GUIComponents/EditMenu.java
@@ -5,9 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.text.BadLocationException;
 
 import me.escapeNT.pail.Util.Localizable;
 import me.escapeNT.pail.Util.ScrollableTextArea;
@@ -47,10 +50,16 @@ public class EditMenu extends JMenu implements Localizable {
         });
         add(find);
 
-        JMenuItem clear = new JMenuItem(Util.translate("Clear"));
+        JMenuItem clear = new JMenuItem(Util.translate("Clear console"));
         clear.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                
+                ScrollableTextArea a = Util.getServerControls().getServerConsolePanel().getConsoleOutput();
+                try {
+                    a.getDocument().remove(0, a.getDocument().getLength());
+                }
+                catch (BadLocationException ex) {
+                    Logger.getLogger(EditMenu.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
         add(clear);
